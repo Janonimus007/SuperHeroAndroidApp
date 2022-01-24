f7ae1a9d119cedd2ef3029a695cf29d7e1adff94
Alejandro <kano.verapad@gmail.com>
2022-01-23 21:38:36 -0300

    add show hero and show information specific

--- src/navigation/StackNavigation.js
+++ src/navigation/StackNavigation.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from '../screens/Home';
+import ShowSuperHero from '../screens/ShowSuperHero';
 const Stack = createNativeStackNavigator();
 const StackNavigation = () => {
   const navigation = useNavigation()
@@ -10,6 +11,11 @@ const StackNavigation = () => {
         <Stack.Screen 
           name="myHome"
           component={Home} 
+          options={{headerShown:false}}
+        />
+        <Stack.Screen 
+          name="showhero"
+          component={ShowSuperHero} 
           options={{headerShown:false}}
         />
       </Stack.Navigator>
