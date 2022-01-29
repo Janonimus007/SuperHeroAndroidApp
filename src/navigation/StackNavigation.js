66b6fcd403e1befcbee91c6a637e49438f0d6bb8
Alejandro <kano.verapad@gmail.com>
2022-01-29 20:58:04 -0300

    adding screen filters

--- src/navigation/StackNavigation.js
+++ src/navigation/StackNavigation.js
@@ -3,6 +3,7 @@ import { useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from '../screens/Home';
 import ShowSuperHero from '../screens/ShowSuperHero';
+import FilterSuperHero from '../screens/FilterSuperHero';
 const Stack = createNativeStackNavigator();
 const StackNavigation = () => {
   const navigation = useNavigation()
@@ -16,6 +17,11 @@ const StackNavigation = () => {
         <Stack.Screen 
           name="showhero"
           component={ShowSuperHero} 
+          options={{headerShown:false}}
+        />
+        <Stack.Screen 
+          name="filter"
+          component={FilterSuperHero} 
           options={{headerShown:false}}
         />
       </Stack.Navigator>
